dd82d0676539b0e87cad0ae92be6777a285eed4a
Deborah <debbiendole@gmail.com>
2024-03-24 19:20:29 +0300

    assignment complete

---
+++ student.js
@@ -0,0 +1,24 @@
+//This function calculates the marks and gives out the corresponding grade
+function grade(marks){
+    if(marks >= 80 && marks <= 100 ){
+      return 'you got an A';
+    }
+    else if(marks >= 60 && marks <= 79 )
+    {
+      return 'you got a B';
+    }
+    else if(marks >= 49 && marks <= 59 )
+    {
+      return 'you got a C';
+    }
+    else if(marks >= 40 && marks <= 49 )
+    {
+      return 'You got a D';
+    }
+    else
+    {
+      return 'you got a E';
+    }
+}
+//constant to show the maximum marks attainable
+const marks = 100;
